ec4498562ff100c593131a3b6dcdf19420f02af6
FattyMieo <fong2009@live.com.my>
2018-06-27 02:01:14 +0800

    Add Monster AI with Preset Waypoints

---
+++ Source/Exilium/Waypoints.cpp
@@ -0,0 +1,27 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#include "Waypoints.h"
+
+
+// Sets default values
+AWaypoints::AWaypoints()
+{
+ 	// Set this actor to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
+	PrimaryActorTick.bCanEverTick = true;
+
+}
+
+// Called when the game starts or when spawned
+void AWaypoints::BeginPlay()
+{
+	Super::BeginPlay();
+	
+}
+
+// Called every frame
+void AWaypoints::Tick(float DeltaTime)
+{
+	Super::Tick(DeltaTime);
+
+}
+
