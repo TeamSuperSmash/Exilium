7ef84f395081ec96ad5de6caf596391fcd584502
Terrence Chan <roxes2112@yahoo.com>
2018-07-15 00:26:34 +0800

    Item Physics Simulation

--- Source/Exilium/Pickup.cpp
+++ Source/Exilium/Pickup.cpp
@@ -5,8 +5,15 @@
 APickup::APickup()
 {
 	PickupMesh = CreateDefaultSubobject<UStaticMeshComponent>("PickupMesh");
+	PickupMesh->SetupAttachment(RootComponent);
 
 	ItemID = FName("Undefined");
 }
 
-
+void APickup::BeginPlay()
+{
+	PickupMesh->SetSimulatePhysics(bSimulatePhysics);
+	PickupMesh->SetEnableGravity(bEnableGravity);
+	PickupMesh->SetLinearDamping(linearDamping);
+	PickupMesh->SetAngularDamping(angularDamping);
+}
