0e7b4bd556f66ba117b8b75a22b0c246ea467dfa
FattyMieo <fong2009@live.com.my>
2018-10-16 18:53:58 +0800

    Remove old codes and rename files
    - Applied a new GameMode for global settings

--- Source/HouseOfIgnotus/HouseOfIgnotus.cpp
+++ Source/HouseOfIgnotus/HouseOfIgnotus.cpp
@@ -1,6 +1,6 @@
 // Fill out your copyright notice in the Description page of Project Settings.
 
-#include "Exilium.h"
+#include "HouseOfIgnotus.h"
 #include "Modules/ModuleManager.h"
 
 IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, HouseOfIgnotus, "HouseOfIgnotus" );
