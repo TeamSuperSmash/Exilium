2b943db25930a5755d66b410dc1fa35b67244a27
Luqman <luqsam1@gmail.com>
2018-11-19 17:23:37 +0800

    Add New BGM
    - Alert BGM
    - Chase BGM
    - Win BGM
    - Change speed mansion door entrace to 1
    - Reduce amplify distance attenuation from 1 - 15 to 1 - 7

--- Source/HouseOfIgnotus/Private/Character/Monster/AI_Bot_Controller.cpp
+++ Source/HouseOfIgnotus/Private/Character/Monster/AI_Bot_Controller.cpp
@@ -199,6 +199,9 @@ void AAI_Bot_Controller::FindPath()
 			if (Player->IsPlayerDangerChaseBGM == true)
 				Player->IsPlayerDangerChaseBGM = false;
 
+			if (Player->IsPlayerDangerAlertBGM == true)
+				Player->IsPlayerDangerAlertBGM = false;
+
 			if(MyCharacter->NextWaypoint != nullptr)
 				NavTarget = MyCharacter->NextWaypoint->GetActorLocation();
 
@@ -220,6 +223,8 @@ void AAI_Bot_Controller::FindPath()
 			bIsPlayerDetected = false;
 
 			Player->IsPlayerDangerChaseBGM = false;
+			Player->IsPlayerDangerAlertBGM = true;
+
 
 			ChaseDuration = 15.0f;
 
@@ -417,15 +422,32 @@ void AAI_Bot_Controller::OnNoiseHeard(APawn* DetectedPawn, const FVector& Locati
 
 		if (Player != nullptr && Player->GetActorLocation() == Location)
 		{
-			if(Player->IsPlayerDangerChaseBGM == false)
-				Player->IsPlayerDangerChaseBGM = true;
-
+			
 			if (FVector::Dist(MyCharacter->GetActorLocation(), Location) <= 120.0f)
 			{
+				//Turn monster state to chase mode
+
+
+				if (Player->IsPlayerDangerAlertBGM == true)
+					Player->IsPlayerDangerAlertBGM = false;
+
+				if (Player->IsPlayerDangerChaseBGM == false)
+					Player->IsPlayerDangerChaseBGM = true;
+
 				bIsPlayerDetected = true;
 				MonsterState = EMonsterState::MS_CHASE;
 
 				ChaseDuration = 8.0f;
+			}
+			else
+			{
+				//Turn monster state to alert mode
+
+				if (Player->IsPlayerDangerChaseBGM == true)
+					Player->IsPlayerDangerChaseBGM = false;
+
+				if (Player->IsPlayerDangerAlertBGM == false)
+					Player->IsPlayerDangerAlertBGM = true;
 			}
 			
 		}
